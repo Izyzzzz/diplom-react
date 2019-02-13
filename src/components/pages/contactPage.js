9e36ae0cf8f4f67bb995388fd16884f07c073551
Serg <izyzzzz@gmail.com>
2019-02-13 10:53:06 +0200

    Form

--- src/components/pages/contactPage.js
+++ src/components/pages/contactPage.js
@@ -1,22 +1,75 @@
 import React, {Component} from 'react';
 import {Container, Col, Row, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import Menu from '../menu';
+import InputMask from 'react-input-mask';
 
 export default class Contact extends Component {
     constructor(props) {
         super(props);
         this.state = {
             name: '',
-            email: ''
+            email: '',
+            phone: '',
+            text: '',
+            redInput: '',
+            textInput: ''
         }
     }
 
     heandleSubmit = (event) => {
         event.preventDefault();
-        console.log("Submit", this.state.name)
+        if(this.state.name.length < 2 || this.state.name.length > 20) {
+          this.setState ({
+            redInput: 'contact__form-input-red'
+          })
+        } else { if (this.state.text.length === 0) {
+          this.setState ({
+            redInput: '',
+            textInput: 'contact__form-input-red'
+          })
+        } else {
+          this.setState ({            
+            textInput: ''
+          })
+          let uploadOb  = {name: this.state.name,
+                          email: this.state.email,
+                          phone: this.state.phone,
+                          text: this.state.text};
+          this.postResource("http://localhost:3001/contacts", uploadOb);
+        }}
+    }
+    handleNameChange = (e) =>{
+      this.setState({name: e.target.value});
+    }
+    handleEmailChange = (e) =>{
+      this.setState({email: e.target.value});
+    }
+    handlePhoneChange = (e) =>{
+      this.setState({phone: e.target.value});
+    }
+    handleTextChange = (e) =>{
+      this.setState({text: e.target.value});
+    }
+    postResource = async (url, body) => {
+      const res = await fetch(url,
+      {
+          method: 'POST',
+          body: JSON.stringify(body),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+        
+      });
+
+      if (!res.ok) {
+          throw new Error(`status: ${res.status}`)
+      }
+      return await res.json();
+    
     }
 
     render() {
+      const {redInput, textInput} = this.state;
         return (
             <>
             <div className="banner">
@@ -43,30 +96,56 @@ export default class Contact extends Component {
                         <FormGroup row >
                           <Label className="contact__form-label" for="exampleName" sm={{size: 2, offset: 3}}>Names<span>*</span></Label>
                           <Col sm={{size: 3, offset: 1}}>
-                            <Input className="contact__form-input" type="text" name="name" id="exampleName" />
+                            <Input
+                              required 
+                              className={`contact__form-input ${redInput}`}
+                              type="text" 
+                              name="name" 
+                              id="exampleName"
+                              onChange={this.handleNameChange}/>
                           </Col>
                         </FormGroup>
                         <FormGroup row>
                           <Label className="contact__form-label" for="exampleEmail" sm={{size: 2, offset: 3}}>Email<span>*</span></Label>
                           <Col sm={{size: 3, offset: 1}}>
-                            <Input className="contact__form-input" type="email" name="email" id="exampleEmail"/>
+                            <Input
+                              required 
+                              className="contact__form-input" 
+                              type="email" 
+                              name="email" 
+                              id="exampleEmail"
+                              onChange={this.handleEmailChange}/>
                           </Col>
                         </FormGroup>
                         <FormGroup row>
                           <Label className="contact__form-label" for="examplePhone" sm={{size: 2, offset: 3}}>Phone</Label>
                           <Col sm={{size: 3, offset: 1}}>
-                            <Input className="contact__form-input" type="phone" name="phone" id="examplePhone"  />
+                            <InputMask
+                              mask="+7 (999) 999-99-99"
+                              className="contact__form-input" 
+                              type="phone" 
+                              name="phone" 
+                              id="examplePhone"
+                              placeholder="+7 (___) ___-__-__"
+                              onChange={this.handlePhoneChange}/>
                           </Col>
                         </FormGroup>
                         <FormGroup row>                      
                           <Col sm={{size: 4, offset: 4}}>
                             <Label className="contact__form-label" for="exampleText" sm={12}>Your message<span>*</span></Label>
-                            <Input className="contact__form-input contact__form-textarea" type="textarea" name="text" id="exampleText" placeholder="Tell us..."/>
+                            <Input 
+                              required
+                              className={`contact__form-input contact__form-textarea ${textInput}`}
+                              type="textarea" 
+                              name="text" 
+                              id="exampleText" 
+                              placeholder="Tell us..."
+                              onChange={this.handleTextChange}/>
                           </Col>
                         </FormGroup>
                         <FormGroup check row>
                           <Col sm={{size: 2, offset: 5}}>
-                            <Button outline color="secondary">Send us</Button>
+                            <Button id="btnSubmin" outline color="secondary">Send us</Button>
                           </Col>
                         </FormGroup>
                     </Form>
